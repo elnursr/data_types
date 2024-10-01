7034b8827be2492f1e6fdc4094560c19a31aa6d6
elnursr <elnursultanv7@gmail.com>
2024-10-01 11:29:54 +0400

    data types with examples in c#

---
+++ data_types/Program.cs
@@ -0,0 +1,40 @@
+﻿//int x = 7;
+//double y = 7.7;
+//string text = "This's string";
+//bool is_false = false;
+//char z = 'z';
+
+//double x = 7;
+//double y = 9;
+
+//double addition = x + y;
+//double subtraction = x - y;
+//double multipication = x * y;
+//double division = x / y;
+
+
+//Console.WriteLine("addition result: " + addition);
+//Console.WriteLine("subtraction result: " + subtraction);
+//Console.WriteLine("multipication result: " + multipication);
+//Console.WriteLine("division result: " + division);
+
+//int a = 5;
+//int b = 3;
+//int remainder = a % b;
+//Console.WriteLine("remainder result: " + remainder);
+
+Console.Write("Please enter your age: ");
+int age = int.Parse(Console.ReadLine());
+
+
+if (age >= 18)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("You can get to exam for driving");
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("You can't get to exam for driving!!!");
+}
